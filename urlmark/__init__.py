d9b7185b6aac850fc3a28dcaf8ea915fc3950a9b
Gu Zhengxiong <rectigu@gmail.com>
2016-02-17 16:01:47 +0800

    UrlMark 0.3.0: Committed.

---
+++ urlmark/__init__.py
@@ -0,0 +1,26 @@
+"""
+Copyright 2015-2016 Gu Zhengxiong <rectigu@gmail.com>
+
+This file is part of UrlMark.
+
+UrlMark is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+UrlMark is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with UrlMark.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+__author__ = 'Gu Zhengxiong'
+__version__ = '0.3.0'
+
+
+PROGRAM_NAME = 'UrlMark'
+PACKAGE_NAME = 'urlmark'
